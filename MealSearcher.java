08d597b75abd8e384205751fdc0530ab2f61597a
Jamila891 <60146134+Jamila891@users.noreply.github.com>
2020-07-13 11:07:21 +0200

    Create MealSearcher.java

---
+++ MealSearcher.java
@@ -0,0 +1,24 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MealSearcher extends Application {
+	
+	private MealSearcherView view;
+	private MealSearcherModel model;
+	private MealSearcherController controller;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		model = new MealSearcherModel();
+		view = new MealSearcherView(stage, model);
+		controller = new MealSearcherController(view, model);
+		
+		view.start();
+	}	
+}
