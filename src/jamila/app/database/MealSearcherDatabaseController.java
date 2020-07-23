a18ebf88379b32876732ffae98a556bf77b12544
jamilamahler <jamilamahler@jamilas-macbook.fritz.box>
2020-07-23 13:15:04 +0200

    saved changes in Controller

--- src/jamila/app/database/MealSearcherDatabaseController.java
+++ src/jamila/app/database/MealSearcherDatabaseController.java
@@ -1,11 +1,14 @@
 package jamila.app.database;
 
+import jamila.app.mealsearcher.Ingredient;
 import jamila.app.mealsearcher.MealSearcherView;
+import jamila.app.mealsearcher.Recipe;
 import javafx.event.ActionEvent;
 
 public class MealSearcherDatabaseController {
 	
 	private MealSearcherView view;
+	private Recipe recipe;
 
 	public void addNewRecipeToDatabase (ActionEvent e) {
 		String name = view.ARnametxt.getText();
@@ -13,6 +16,8 @@ public class MealSearcherDatabaseController {
 		String url = view.ARurltxt.getText();
 		String amount = view.ARamountxt.getText();
 		String ingredient = view.ARingredienttxt.getText();
+		
+		recipe = new Recipe (name, instructions, url, new Ingredient(amount, ingredient));
 		
 //		String qu = "INSERT INTO MEALSEARCHER (" +
 //		" ' " + RecipeNo +
